3c70ee3d58b37e44a70eb2ed2a2aa5c32f75ee95
MoosaAfzal2 <110198103+MoosaAfzal2@users.noreply.github.com>
2022-10-07 00:56:02 +0500

    Add files via upload

---
+++ JavaScript_45_Excersizes/Guests.js
@@ -0,0 +1,33 @@
+const Guests = ["Umar", "Ali", "Areeb"]
+for (var i in Guests) {
+    console.log(`Hello ${Guests[i]}, Would you like to Come to Dinner Today?`)
+}
+console.log("\nUmar cannot Come to Dinner.\n")
+
+Guests[0] = "Imran"
+for (var i in Guests) {
+    console.log(`Hello ${Guests[i]}, Would you like to Come to Dinner Today?`)
+}
+console.log("\nI have Found a Bigger Dinner Table!\n")
+
+Guests.unshift("Kamran")
+Guests.splice(2, 0, "Mustafa")
+Guests.push("Abdullah")
+
+for (var i in Guests) {
+    console.log(`Hello ${Guests[i]}, Would you like to Come to Dinner Today?`)
+}
+
+console.log("\nI can only invite Two people to Dinner, Sorry!\n")
+
+const not_invited = Guests.splice(2)
+for (var i in Guests) {
+    console.log(`Hello ${not_invited[i]}, I am sorry i can only invite two People`)
+}
+
+for (var i in Guests) {
+    console.log(`Hello ${Guests[i]}, You are Still Invited`)
+}
+
+Guests.splice(0, 2)
+console.log(Guests)
