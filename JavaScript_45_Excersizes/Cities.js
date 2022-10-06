fa4650160406275a34ca59bf75cc6c9fc4a359a6
MoosaAfzal2 <110198103+MoosaAfzal2@users.noreply.github.com>
2022-10-07 01:58:08 +0500

    Add files via upload

---
+++ JavaScript_45_Excersizes/Cities.js
@@ -0,0 +1,7 @@
+function Describe_City(City = "Karachi", Country = "Pakistan") {
+    console.log(City, "is a city of", Country)
+}
+
+Describe_City()
+Describe_City("Islamabad")
+Describe_City("Hyderabad")
---
+++ JavaScript_45_Excersizes/City_Names.js
@@ -0,0 +1,8 @@
+function City_Country(City, Country = "Pakistan") {
+    var String = City + "," + Country
+    return String
+}
+
+console.log(City_Country("Karachi"))
+console.log(City_Country("Islamabad"))
+console.log(City_Country("Lahore"))
