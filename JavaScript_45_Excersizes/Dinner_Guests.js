96c776aca263df6053bbf9e18048da681ffb73c6
MoosaAfzal2 <110198103+MoosaAfzal2@users.noreply.github.com>
2022-10-07 01:33:12 +0500

    Add files via upload

---
+++ JavaScript_45_Excersizes/Dinner_Guests.js
@@ -0,0 +1,5 @@
+const Guests = ["Umar", "Ali", "Areeb"]
+for (var i in Guests) {
+    console.log(`Hello ${Guests[i]}, Would you like to Come to Dinner Today?`)
+}
+console.log("\nThe Number of Guests Invited is:", Guests.length)
