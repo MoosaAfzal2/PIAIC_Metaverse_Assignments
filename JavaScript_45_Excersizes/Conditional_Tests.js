dd309675dd9a62cf19e82e247128cae984d93a9f
MoosaAfzal2 <110198103+MoosaAfzal2@users.noreply.github.com>
2022-10-07 01:48:35 +0500

    Add files via upload

---
+++ JavaScript_45_Excersizes/Conditional_Tests.js
@@ -0,0 +1,30 @@
+const Car = "Honda"
+console.log("is Porche = Car")
+console.log("porsche" == Car)
+
+console.log("is Ferrari = Car")
+console.log("Ferrari" == Car)
+
+console.log("is Lamborghni = Car")
+console.log("lamborghni" == Car)
+
+console.log("is Khyber = Car")
+console.log("khyber" == Car)
+
+console.log("is Ford = Car")
+console.log("Ford" == Car)
+
+console.log("is Honda = Car")
+console.log("Honda" == Car)
+
+console.log("is Porche not = Car")
+console.log("porsche" != Car)
+
+console.log("is Honda = Car")
+console.log("Honda" == Car)
+
+console.log("is Honda = Car")
+console.log("Honda" == Car)
+
+console.log("is Honda = Car")
+console.log("Honda" == Car)
